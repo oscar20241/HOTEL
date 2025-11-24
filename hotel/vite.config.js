afcc7b6eff176e9165d066b3f27745a7b53c6a6b
Oscar_Hernandez <ti1901056@academica.utbb.edu.mx>
2025-11-24 17:28:58 -0600

    Fix vite: agregar estilo.css, gerente.css, etc.

--- hotel/vite.config.js
+++ hotel/vite.config.js
@@ -4,7 +4,17 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+
+                // ➕ Tus CSS reales que usas en @vite()
+                'resources/css/estilo.css',
+                'resources/css/estilo2.css',
+                'resources/css/gerente.css',
+                'resources/css/recepcionista.css',
+                'resources/css/huesped.css',
+            ],
             refresh: true,
         }),
     ],
